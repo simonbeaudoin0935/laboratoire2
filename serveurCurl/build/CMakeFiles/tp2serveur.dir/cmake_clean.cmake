3fa07354c46932c34886b191261920ebaab420f8
Simon Beaudoin <simon.beaudoin.0935@gmail.com>
2018-02-16 13:36:49 -0500

    W

---
+++ serveurCurl/build/CMakeFiles/tp2serveur.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tp2serveur.dir/telechargeur.c.o"
+  "CMakeFiles/tp2serveur.dir/requete.c.o"
+  "CMakeFiles/tp2serveur.dir/actions.c.o"
+  "CMakeFiles/tp2serveur.dir/servermain.c.o"
+  "CMakeFiles/tp2serveur.dir/communications.c.o"
+  "tp2serveur.pdb"
+  "tp2serveur"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/tp2serveur.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
